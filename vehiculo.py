b46ce771c79949dbb8b0560a1e1429a9103782fd
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-07 21:44:58 -0300

    feat: agregar atributos patente, anio y _en_taller a Vehiculo

--- vehiculo.py
+++ vehiculo.py
@@ -1,4 +1,8 @@
-# Declaración de la clase Vehiculo que servirá como molde o plantilla
+# Declaración de la clase Vehiculo
 class Vehiculo:
-    # Sentencia 'pass' que actúa como marcador de posición para mantener la clase vacía sin generar error sintáctico
-    pass
+    # Atributo de tipo texto (str) para la patente del vehículo
+    patente: str
+    # Atributo de tipo entero (int) para el año de fabricación
+    anio: int
+    # Atributo booleano (bool) protegido que indica si el vehículo está en el taller
+    _en_taller: bool
