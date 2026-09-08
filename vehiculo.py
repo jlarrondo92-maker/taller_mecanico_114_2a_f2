2c126fd284eb89d1a5fbd45baf3a9a4d49c3eba4
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-07 21:35:22 -0300

    feat: agregar clase Vehiculo en vehiculo.py

---
+++ vehiculo.py
@@ -0,0 +1,4 @@
+# Declaración de la clase Vehiculo que servirá como molde o plantilla
+class Vehiculo:
+    # Sentencia 'pass' que actúa como marcador de posición para mantener la clase vacía sin generar error sintáctico
+    pass
